11309fbf3da9f330a8d6e2fccf0752d1b5cbc04e
mariacamachos <maria.camachos@edu.uah.es>
2025-02-21 12:54:04 +0100

    Authorprueba

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,21 @@
-//importo la clase Ball
+import es.uah.matcomp.mp.e2.ejerciciosclases.primeroprueba.Authorprueba;
 
-import es.uah.matcomp.mp.e1.ejerciciosclases.e9.Ball;
 public class Main {
     public static void main(String[] args) {
-        //creo una bola y la devuelvo con el toString
-        Ball ball = new Ball(1.1f, 2.2f, 10, 3.3f, 4.4f);
-        System.out.println(ball);
-        //doy valor a x, y , radio, Xdelta, YDelta y los devuelvo con el metodo toString
-        ball.setX(80.0f);
-        ball.setY(35.0f);
-        ball.setRadius(5);
-        ball.setXDelta(4.0f);
-        ball.setYDelta(6.0f);
-        System.out.println(ball);
-        //devuelvo los valores del x, y, radio, XDelta y del YDelta
-        System.out.println("x is: " + ball.getX());
-        System.out.println("y is: " + ball.getY());
-        System.out.println("radius is: " + ball.getRadius());
-        System.out.println("xDelta is: " + ball.getXDelta());
-        System.out.println("yDelta is: " + ball.getYDelta());
-        //pruebo el move
-        float xMin = 0.0f;
-        float xMax = 100.0f;
-        float yMin = 0.0f;
-        float yMax = 50.0f;
-        for (int i = 0; i < 15; i++) {
-            ball.move();
-            System.out.println(ball);
-            float xNew = ball.getX();
-            float yNew = ball.getY();
-            int radius = ball.getRadius();
-            //
-            if ((xNew + radius) > xMax || (xNew - radius) < xMin) {
-                ball.reflectHorizontal();
-            }
-            if ((yNew + radius) > yMax || (yNew - radius) < yMin) {
-                ball.reflectVertical();
-            }
-        }
+        /**Establezco los datos del autor*/
+        Authorprueba ahTeck = new Authorprueba("Tan Ah Teck", "ahteck@nowhere.com", 'm');
+        /**Imprimo los datos del autor por pantalla*/
+        System.out.println(ahTeck);
+
+        /**A traves de un setter, establezco un nuevo correo*/
+        ahTeck.setEmail("paulTan@nowhere.com");
+        /**Imprime el nombre del autor */
+        System.out.println("El nombre es: " + ahTeck.getName());
+        /**Imprime el email establecido a traves del setter*/
+        System.out.println("El email es: " + ahTeck.getEmail());
+        /**Imprime el genero, en el caso de ser masculino será un 'm' y en caso de ser femenino
+         * se imprimirá un 'f' en el caso de gender
+         */
+        System.out.println("Su genero es: " + ahTeck.getGender());
     }
-
 }
-
